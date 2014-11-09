d36f1849ab160d6c00353a9f6cf9afb716f9b61e
mrabins <markrabins@gmail.com>
2014-11-08 20:32:10 -0800

    added calculation code

--- Age of Laika/ViewController.swift
+++ Age of Laika/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    @IBOutlet weak var humanYearsTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,39 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        let stringFromTextField = humanYearsTextField.text
+        let optionalIntFromTextField = stringFromTextField.toInt()
+        let intFromOptional = optionalIntFromTextField!
+        
+        dogYearsLabel.hidden = false
+        dogYearsLabel.text = "\(intFromOptional * 7)" + " Human Years"
+        
+        humanYearsTextField.text = " "
+        humanYearsTextField.resignFirstResponder()
+        
+    }
 
+    @IBAction func convertRealDogYearsButtonPressed(sender: UIButton) {
+        let stringFromTextField = humanYearsTextField.text
+        let doubleFromTextField = Double ((stringFromTextField as NSString).doubleValue)
+        
+        var realDogYears:Double
+        
+        if doubleFromTextField > 2 {
+            realDogYears = (10.5 * 2) + (doubleFromTextField - 2) * 4
+        }
+        else {
+            realDogYears = doubleFromTextField * 10.5
+        }
+        
+        dogYearsLabel.hidden = false
+        dogYearsLabel.text = "\(realDogYears)" + " Real Human Years"
+        
+        humanYearsTextField.text = " "
+        humanYearsTextField.resignFirstResponder()
+    }
 
 }
 
